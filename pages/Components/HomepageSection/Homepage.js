4c0701fddc358d77b01380dc3b635130b37e14ae
madhursharma026 <105696243+madhursharma026@users.noreply.github.com>
2023-04-10 14:00:00 +0530

    Update!

--- pages/Components/HomepageSection/Homepage.js
+++ pages/Components/HomepageSection/Homepage.js
@@ -13,111 +13,118 @@ function Homepage() {
 
     return (
         <>
-            <div className="container mt-5">
-                <div>
-                    <div className={`row ${styles.content}`}>
-                        <div className={`col-lg-7 ${styles.contentSide}`}>
-                            <div className={`${styles.contentSideHeading}`} style={{ fontSize: "calc(6vw + 1.5rem)", fontWeight: "500", lineHeight: "1em", }}>
-                                About Us
-                            </div>
-                            <p className={`${styles.contentSideParagraph} mt-3`} style={{ fontSize: "calc(0.95vw + 1rem)", fontWeight: "500", lineHeight: "calc(0.95vw + 1rem)", }}>
-                                LR Energy is India’s leading generator of renewable energy with a proven track record in delivering high performance cost competitive solar photovoltaic plants and economic biogas production to the market. The company is dedicated to promoting environmental, economic, and social sustainability with clean energy offerings for today and our future generations. In today’s world of climbing fossil fuel prices, approaching the peak oil supply limit, and discussions of global warming, Paris Agreement- our aim is to improve the quality of life and promote sustainable development throughout the nation by looking for new opportunities across multiple regulatory regimes.
-                            </p>
-                            <Link href="/aboutus" className={`${styles.btnStyle} ${styles.contentSideBtn} mt-4`} style={{ textDecoration: 'none' }}>More About Us</Link>
-                        </div>
-                        <div className={`col-lg-5 ${styles.imageSide}`} style={{ margin: "auto" }}>
-                            <img src="https://sunrix.co.in/wp-content/uploads/2019/11/bdb6d217af1dc43bd022cb8168715234.gif" alt="#ImgNotFound" className="w-100" />
-                            {/* <img src="../../../factBg.jpg" alt="#ImgNotFound" className="w-100" /> */}
-                        </div>
-                    </div>
-                </div>
+            <div className="px-3 px-sm-5 mx-md-5 mt-5">
+                {/* <div className="container mt-5"> */}
                 <div className="row mt-3">
-                    <div className={`col-lg-4 ${styles.colStyle1}`}>
-                        <div class="card" style={{ overflow: "hidden" }}>
-                            <div className={`${styles.block} ${styles.imgStyle}`} style={{ backgroundImage: "url(https://l-renergy.vercel.app/ourMissionImg.jpg)", minHeight: '400px', width: '100%', }}>
-                                <div class={`${styles.centered} p-3`}>
-                                    <h1 className="text-center">Our Mission</h1>
-                                    <h5 style={{ textAlign: 'justify' }}>
-                                        Powering change by ensuring that we become a leader in the low carbon industry. Through LR Energy, we will build a culture that allows our people to integrate sustainability into every part of our day-to-day operations.
-                                    </h5>
+                    <div className={`text-center`} style={{ fontSize: "calc(6vw + 1.5rem)", fontWeight: "500", lineHeight: "1em", }}>
+                        About Us
+                    </div>
+                    <div className={`col-xxl-4 ${styles.content}`}>
+                        <p className={`${styles.contentSideParagraph} mt-3`} style={{ fontSize: "24px", fontWeight: "500", lineHeight: "1em" }}>
+                            LR Energy is India’s leading generator of renewable energy with a proven track record in delivering high performance cost competitive solar photovoltaic plants and economic biogas production to the market. The company is dedicated to promoting environmental, economic, and social sustainability with clean energy offerings for today and our future generations. In today’s world of climbing fossil fuel prices, approaching the peak oil supply limit, and discussions of global warming, Paris Agreement- our aim is to improve the quality of life and promote sustainable development throughout the nation by looking for new opportunities across multiple regulatory regimes.
+                        </p>
+                        <Link href="/aboutus" className={`${styles.btnStyle} mt-4`} style={{ textDecoration: 'none' }}>More About Us</Link>
+                    </div>
+                    <div className={`col-xxl-8 ${styles.colStyle1}`}>
+                        <div className="row">
+                            <div className={`col-lg-4 ${styles.colStyle1}`}>
+                                <div class="card" style={{ overflow: "hidden", border:'5px solid black' }}>
+                                    <div className={`${styles.block} ${styles.imgStyle}`} style={{ backgroundImage: "url(https://l-renergy.vercel.app/ourMissionImg.jpg)", minHeight: '400px', width: '100%', }}>
+                                        <div class={`${styles.centered} p-3`}>
+                                            <h1 className="text-center">Our Mission</h1>
+                                            <h5 style={{ textAlign: 'justify' }}>
+                                                Powering change by ensuring that we become a leader in the low carbon industry. Through LR Energy, we will build a culture that allows our people to integrate sustainability into every part of our day-to-day operations.
+                                            </h5>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                    <div className={`col-lg-4 ${styles.colStyle2}`}>
-                        <div class="card" style={{ overflow: "hidden" }}>
-                            <div className={`${styles.block} ${styles.imgStyle}`} style={{ backgroundImage: "url(https://l-renergy.vercel.app/ourValuesImg.jpg)", minHeight: '400px', width: '100%', }}>
-                                <div class={`${styles.centered} p-3`}>
-                                    <h1 className="text-center">Our Mission</h1>
-                                    <h5 style={{ textAlign: 'justify' }}>
-                                        Help each other become effective, Integrity in our day-to-day working, Driving excellence and Be ethical.
-                                    </h5>
+                            <div className={`col-lg-4 ${styles.colStyle2}`}>
+                                <div class="card" style={{ overflow: "hidden", border:'5px solid black' }}>
+                                    <div className={`${styles.block} ${styles.imgStyle}`} style={{ backgroundImage: "url(https://l-renergy.vercel.app/ourValuesImg.jpg)", minHeight: '400px', width: '100%', }}>
+                                        <div class={`${styles.centered} p-3`}>
+                                            <h1 className="text-center">Our Values</h1>
+                                            <h5 style={{ textAlign: 'justify' }}>
+                                                Help each other become effective, Integrity in our day-to-day working, Driving excellence and Be ethical.
+                                            </h5>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                    <div className={`col-lg-4 ${styles.colStyle3}`}>
-                        <div class="card" style={{ overflow: "hidden" }}>
-                            <div className={`${styles.block} ${styles.imgStyle}`} style={{ backgroundImage: "url(https://l-renergy.vercel.app/ourVisionImg.jpg)", minHeight: '400px', width: '100%', }}>
-                                <div class={`${styles.centered} p-3`}>
-                                    <h1 className="text-center">Our Mission</h1>
-                                    <h5 style={{ textAlign: 'justify' }}>
-                                        To build the best renewable energy company by providing clean, green and secure energy for the benefit of the environment and our communities.
-                                    </h5>
+                            <div className={`col-lg-4 ${styles.colStyle3}`}>
+                                <div class="card" style={{ overflow: "hidden", border:'5px solid black' }}>
+                                    <div className={`${styles.block} ${styles.imgStyle}`} style={{ backgroundImage: "url(https://l-renergy.vercel.app/ourVisionImg.jpg)", minHeight: '400px', width: '100%', }}>
+                                        <div class={`${styles.centered} p-3`}>
+                                            <h1 className="text-center">Our Vision</h1>
+                                            <h5 style={{ textAlign: 'justify' }}>
+                                                To build the best renewable energy company by providing clean, green and secure energy for the benefit of the environment and our communities.
+                                            </h5>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <div className="text-center mt-5 pt-md-5">
-                    <div className={`${styles.mainHeading}`} style={{ fontSize: 'calc(6vw + 1.5rem)' }}>
-                        SERVICES
+                {/* <div className="container-lg mt-5"> */}
+                    <div className="text-center mt-5 pt-md-5">
+                        <div className={`${styles.mainHeading}`} style={{ fontSize: 'calc(6vw + 1.5rem)' }}>
+                            SERVICES
+                        </div>
+                        <p className={`text-center ${styles.paragraph}`}>
+                            We combine a balanced portfolio of technologically advanced large-scale assets with outstanding technical and commercial expertise.
+                        </p>
                     </div>
-                    <p className={`${styles.paragraph}`}>
-                        We combine a balanced portfolio of technologically advanced large-scale assets with outstanding technical and commercial expertise.
-                    </p>
-                </div>
-                <div className="row">
-                    <div className="col-md-4 mt-3">
-                        <h4>
-                            {/* <img src={solarImg.src} alt="#ImgNotFound" width="40px" /> */}
-                            <img src='../../../solarImg.png' alt="ImgNotFound" width="40px" />
-                            &ensp;
-                            <Link href="/solarPowerSolution" style={{ textDecoration: 'none', color: "black" }}>
-                                <b>Solar Power Solutions</b>
-                            </Link>
-                        </h4>
-                        <p class="card-text">With rising power prices and reducing amounts of power available for the increasing population, it would make sense to go solar. Our objective is to generate clean solar electricity, focusing on the most secure portion of the solar PV value chain: power plant development, financing, construction and asset management.</p>
-                        <Link href="/solarPowerSolution" className={`${styles.btnStyle}`} style={{ textDecoration: 'none' }}>Know More</Link>
-                    </div>
-                    <div className="col-md-4 mt-3">
-                        <h4>
-                            {/* <img src={bioGasImg.src} alt="#ImgNotFound" width="40px" /> */}
-                            <img src='../../../bioGasImg.png' alt="ImgNotFound" width="40px" />
-                            &ensp;
-                            <Link href="/bioGasSolution" style={{ textDecoration: 'none', color: "black" }}>
-                                <b>Bio-Gas Solutions</b>
-                            </Link>
-                        </h4>
-                        <p class="card-text">The company’s strategy is based on generating and delivering clean energy and building renewable sources that are abundant, affordable and accessible. So, we focus on a high biogas production, low operational costs, low maintenance costs and a high flexibility in the biomasses that can be used in the biogas plant.</p>
-                        <Link href="/bioGasSolution" className={`${styles.btnStyle}`} style={{ textDecoration: 'none' }}>Know More</Link>
-                    </div>
-                    <div className="col-md-4 mt-3">
-                        <h4>
-                            {/* <img src={agriImg.src} alt="#ImgNotFound" width="40px" /> */}
-                            <img src='../../../agriImg.png' alt="ImgNotFound" width="40px" />
-                            &ensp;
-                            <Link href="/agriSolution" style={{ textDecoration: 'none', color: "black" }}>
-                                <b>Agri Solutions</b>
-                            </Link>
-                        </h4>
-                        <p class="card-text">As a premier recycler of organic residuals throughout Northern part of INDIA , LR Agri proudly manufactures Bio-enriched Compost & PROM.  Our company and products are known for quality, consistency, and value throughout the landscape, construction, and agricultural industries.</p>
-                        <Link href="/agriSolution" className={`${styles.btnStyle}`} style={{ textDecoration: 'none' }}>Know More</Link>
+                    <div className="row">
+                        <div className="col-lg-4 mt-3">
+                            <div class="card h-100" style={{ boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px", border: 'none' }}>
+                                <div class="card-body">
+                                    <h4>
+                                        <img src='../../../solarImg.png' alt="ImgNotFound" width="80px" className="mb-3" />
+                                        <br />
+                                        <Link href="/solarPowerSolution" style={{ textDecoration: 'none', color: "black" }}>
+                                            <b>Solar Power Solutions</b>
+                                        </Link>
+                                    </h4>
+                                    <p class="card-text">With rising power prices and reducing amounts of power available for the increasing population, it would make sense to go solar. Our objective is to generate clean solar electricity, focusing on the most secure portion of the solar PV value chain: power plant development, financing, construction and asset management.</p>
+                                    <Link href="/solarPowerSolution" className={`${styles.btnStyle}`} style={{ textDecoration: 'none' }}>Know More</Link>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 mt-3">
+                            <div class="card h-100" style={{ boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px", border: 'none' }}>
+                                <div class="card-body">
+                                    <h4>
+                                        <img src='../../../bioGasImg.png' alt="ImgNotFound" width="80px" className="mb-3" />
+                                        <br />
+                                        <Link href="/bioGasSolution" style={{ textDecoration: 'none', color: "black" }}>
+                                            <b>Bio-Gas Solutions</b>
+                                        </Link>
+                                    </h4>
+                                    <p class="card-text">The company’s strategy is based on generating and delivering clean energy and building renewable sources that are abundant, affordable and accessible. So, we focus on a high biogas production, low operational costs, low maintenance costs and a high flexibility in the biomasses that can be used in the biogas plant.</p>
+                                    <Link href="/bioGasSolution" className={`${styles.btnStyle}`} style={{ textDecoration: 'none' }}>Know More</Link>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 mt-3">
+                            <div class="card h-100" style={{ boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px", border: 'none' }}>
+                                <div class="card-body">
+                                    <h4>
+                                        <img src='../../../agriImg.png' alt="ImgNotFound" width="80px" className="mb-3" />
+                                        <br />
+                                        <Link href="/agriSolution" style={{ textDecoration: 'none', color: "black" }}>
+                                            <b>Agri Solutions</b>
+                                        </Link>
+                                    </h4>
+                                    <p class="card-text">As a premier recycler of organic residuals throughout Northern part of INDIA , LR Agri proudly manufactures Bio-enriched Compost & PROM.  Our company and products are known for quality, consistency, and value throughout the landscape, construction, and agricultural industries.</p>
+                                    <Link href="/agriSolution" className={`${styles.btnStyle}`} style={{ textDecoration: 'none' }}>Know More</Link>
+                                </div>
+                            </div>
+                        {/* </div> */}
                     </div>
                 </div>
             </div>
-
             <div class={`${styles.section} mt-5`}></div>
             <div class={`container ${styles.content1} text-center`}>
                 <div className="row">
@@ -163,7 +170,7 @@ function Homepage() {
                 <SolarCalculatorSection />
             </div>
 
-            <div className="mt-5" style={{ background: '#18d26e', border: '20px solid #767676' }}>
+            <div className="mt-5" style={{ backgroundImage: 'url(https://amplussolar.com/newAmplus/images/amplus-solar-testimonials-bg.jpg)', border: '20px solid #767676' }}>
                 <div class={`${styles.bgSection} mt-5`}></div>
                 <div class={`container ${styles.bgSectionContent} text-center`}>
                     <div className="row p-sm-5 p-3">
--- pages/Components/OurProject.js
+++ pages/Components/OurProject.js
@@ -11,16 +11,46 @@ function OurProjects() {
         <>
             <div className="container">
                 <div className="text-center mt-5">
-                    <div className={`${styles.mainHeading}`} style={{fontSize:'calc(6vw + 1.5rem)'}}>
+                    <div className={`${styles.mainHeading}`} style={{ fontSize: 'calc(6vw + 1.5rem)' }}>
                         OUR PROJECTS
                     </div>
                     <Link href="/" className={`${styles.btnStyle}`} style={{ textDecoration: 'none' }}>ALL</Link>
                 </div>
                 <div className="row">
-                    <div className="col-md-4 mt-4">
-                        <div class="card" style={{ overflow: "hidden" }}>
-                            {/* <img src={sirsaProject.src} className={`card-img-top ${styles.imgStyle}`} alt="ImgNotFound" /> */}
-                            <img src="/sirsaProject.jpg" className={`card-img-top ${styles.imgStyle}`} alt="ImgNotFound" />
+                    <div className="col-md-6">
+                        <div class="card mt-3" style={{ overflow: "hidden", border:'5px solid black' }}>
+                            <img src="/sirsaProject.jpg" className={`card-img-top ${styles.imgStyle}`} alt="ImgNotFound" style={{maxHeight:'400px'}} />
+                            <div class="card-body text-center">
+                                <h4>
+                                    <b>12 MW Solar Project</b>
+                                </h4>
+                                <p class="card-text"><b>SIRSA, HARYANA</b></p>
+                            </div>
+                        </div>
+                        <div class="card mt-3" style={{ overflow: "hidden", border:'5px solid black' }}>
+                            <img src="/mangaliProject.jpg" className={`card-img-top ${styles.imgStyle}`} alt="ImgNotFound" style={{maxHeight:'400px'}} />
+                            <div class="card-body text-center">
+                                <h4>
+                                    <b>12MW Solar Project</b>
+                                </h4>
+                                <p class="card-text"><b>MANGALI, HARYANA</b></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={`col-md-6 ${styles.marginTop}`}>
+                        <div class="card mt-3" style={{ overflow: "hidden", border:'5px solid black' }}>
+                            <img src="/toshamProject.jpg" className={`card-img-top ${styles.imgStyle}`} alt="ImgNotFound" style={{maxHeight:'400px'}} />
+                            <div class="card-body text-center">
+                                <h4>
+                                    <b>24 MW Solar Project</b>
+                                </h4>
+                                <p class="card-text"><b>TOSHAM, HARYANA</b></p>
+                            </div>
+                        </div>
+                    </div>
+                    {/* <div className="col-md-4 mt-4">
+                        <div class="card" style={{ overflow: "hidden", border:'5px solid black' }}>
+                            <img src="/sirsaProject.jpg" className={`card-img-top ${styles.imgStyle}`} alt="ImgNotFound" style={{maxHeight:'400px'}} />
                             <div class="card-body text-center">
                                 <h4>
                                     <b>12 MW Solar Project</b>
@@ -30,9 +60,8 @@ function OurProjects() {
                         </div>
                     </div>
                     <div className={`col-md-4 ${styles.colStyle2}`}>
-                        <div class="card" style={{ overflow: "hidden" }}>
-                            {/* <img src={mangaliProject.src} className={`card-img-top ${styles.imgStyle}`} alt="ImgNotFound" /> */}
-                            <img src="/mangaliProject.jpg" className={`card-img-top ${styles.imgStyle}`} alt="ImgNotFound" />
+                        <div class="card" style={{ overflow: "hidden", border:'5px solid black' }}>
+                            <img src="/mangaliProject.jpg" className={`card-img-top ${styles.imgStyle}`} alt="ImgNotFound" style={{maxHeight:'400px'}} />
                             <div class="card-body text-center">
                                 <h4>
                                     <b>12MW Solar Project</b>
@@ -42,9 +71,8 @@ function OurProjects() {
                         </div>
                     </div>
                     <div className="col-md-4 mt-4">
-                        <div class="card" style={{ overflow: "hidden" }}>
-                            {/* <img src={toshamProject.src} className={`card-img-top ${styles.imgStyle}`} alt="ImgNotFound" /> */}
-                            <img src="/toshamProject.jpg" className={`card-img-top ${styles.imgStyle}`} alt="ImgNotFound" />
+                        <div class="card" style={{ overflow: "hidden", border:'5px solid black' }}>
+                            <img src="/toshamProject.jpg" className={`card-img-top ${styles.imgStyle}`} alt="ImgNotFound" style={{maxHeight:'400px'}} />
                             <div class="card-body text-center">
                                 <h4>
                                     <b>24 MW Solar Project</b>
@@ -52,7 +80,7 @@ function OurProjects() {
                                 <p class="card-text">TOSHAM, HARYANA</p>
                             </div>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </>
